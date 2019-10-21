82df0c1dd33513e8029f64722c41547d2c91bb6f
William Stevens <wstevens.hornet@gmail.com>
2019-10-21 06:39:26 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,7 +3,10 @@ class CashRegister
   attr_accessor :total :discount :price :item 
   
   def initialize(discount = 0)
-    @
+    @total = 0
+    @discount = discount
+    @items = []
+  end
   
   
   
